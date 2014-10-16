68604370819130b31494eff3bcd527a802788414
Gopakumar.T <gopakumar@qburst.com>
2014-10-16 10:58:57 +0530

    adding a sample java project to print Hello world

---
+++ projectrt/src/main/java/com/projectRT/App.java
@@ -0,0 +1,13 @@
+package com.projectRT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
